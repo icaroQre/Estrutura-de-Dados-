dd87a837c6ac87e2d275c0dc312e88837b21996a
Ícaro Queiroz <107288351+icaroQre@users.noreply.github.com>
2022-06-26 15:45:36 -0300

    Update Main.java

--- Lista de exercícios 02/Ex02/Main.java
+++ Lista de exercícios 02/Ex02/Main.java
@@ -1,3 +1,8 @@
+//Escrever um algoritmo que lê um vetor G (13) que é o gabarito de um teste de loteria esportiva, contendo os valores 1 (coluna 1) ou 2 (coluna 2) ou 3 (coluna do meio).
+//Ler, a seguir, para cada apostador, o número de seu cartão e um vetor Resposta R (13). Verificar para cada apostador o número de acertos e escrever o número do apostador e seu número de acertos. 
+//Se tiver 13 acertos, acrescentar a mensagem: "GANHADOR, PARABENS".
+
+
 import java.util.Scanner;
 
 public class Main {
