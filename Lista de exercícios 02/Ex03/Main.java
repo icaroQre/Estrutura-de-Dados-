77e686921c093848c93458da163ecedf79308d37
Ícaro Queiroz <107288351+icaroQre@users.noreply.github.com>
2022-06-26 15:46:18 -0300

    Update Main.java

--- Lista de exercícios 02/Ex03/Main.java
+++ Lista de exercícios 02/Ex03/Main.java
@@ -1,3 +1,12 @@
+//Escreva um algoritmo que lê uma matriz M (5,5) e calcule as somas:
+
+//a) da linha 4 de M;
+//b) da coluna 2 de M;
+//c) da diagonal principal;
+//d) da diagonal secundária;
+//e) de todos os elementos da matriz;
+//f) Escreva estas somas e a matriz;
+
 public class Main {
     public static void main(String[] args) {
 
