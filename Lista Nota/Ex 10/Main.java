5be914d3811af97f1c2afa2a915813c68dc65d44
Ícaro Queiroz <107288351+icaroQre@users.noreply.github.com>
2022-07-25 20:27:15 -0300

    Create Main.java

---
+++ Lista Nota/Ex 10/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    /*
+
+    Transforme cada uma das extensões pré-fixas em pós-fixas (notação polonesa
+    reversa):
+    ex. : A+B+C => AB+C+;
+   //a A+B-C;
+   //R: AB+C-
+    
+   //b (A+B)*(C-D) / (E*F);
+   //R: AB+CD-*EF*/
+   
+    //c {(A-B)/(C*D)}/E;
+    //R: AB-CD*/E/
+    
+    //d (A+ {[(B-C)*(D-E)+F]/G})/(H-J);
+    //R: BC-DE-*F+G/A+HJ-/ 
+
+}
