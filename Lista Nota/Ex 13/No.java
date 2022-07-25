adc077cd7d41e07f710ca9bc1be058c23f28ebd4
Ícaro Queiroz <107288351+icaroQre@users.noreply.github.com>
2022-07-25 20:32:18 -0300

    Create No.java

---
+++ Lista Nota/Ex 13/No.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lista1.exercicio13;
+
+/**
+ *
+ * @author leona
+ */
+public class No {
+    
+    String nome;
+    int id;
+
+    public No(String n, int i){
+        this.nome = n;
+        this.id = i;
+    }
+
+    public String tostring(){
+        return  "nome: "+nome+" id: "+id;
+    }
+    
+    
+}
