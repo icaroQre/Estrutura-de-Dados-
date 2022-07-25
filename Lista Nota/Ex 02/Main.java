2a850d3381bee796287c0324ed8a9ed636a499b2
Ícaro Queiroz <107288351+icaroQre@users.noreply.github.com>
2022-07-25 20:36:42 -0300

    Create Main.java

---
+++ Lista Nota/Ex 02/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    /*
+    
+    Pergunta: Explique o que é um tipo de dado primitivo e o que é um tipo de dado abstrato.
+    Dê exemplos. Uma Classe é de que tipo de dados?
+    
+    Resposta: Tipo de dado primitivo é indivisível, ou seja, uma vez alocado, não pe possível modificar o tipo.
+    Exemplo: in, long sheet e etc.
+    Tipo de dados abstrato são generalizações que descrevem ações para os dados, classes é um exemplo
+    de tipo de dado abstrato.
+    
+    */ 
+}
