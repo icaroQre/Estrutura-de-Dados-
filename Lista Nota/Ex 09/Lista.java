01ad0ada11a22bb9f9b497d0292c69fdbb4c1f91
Ícaro Queiroz <107288351+icaroQre@users.noreply.github.com>
2022-07-25 20:33:41 -0300

    Create Lista.java

---
+++ Lista Nota/Ex 09/Lista.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lista1.exercicio9;
+
+/**
+ *
+ * @author leona
+ */
+public class Lista {
+    
+    int indice;
+    int valor;
+    
+    public Lista(int indice, int valor){
+        
+        this.indice = indice;
+        this.valor = valor;
+        
+    }
+    
+    public String toString(){
+        
+        return "Indice: "+this.indice+" Valor: "+this.valor;
+    
+    }
+    
+    
+    
+}
