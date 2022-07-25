5bc9324ea3f9f5a9ae4e7e866f942cabdee0138b
Ícaro Queiroz <107288351+icaroQre@users.noreply.github.com>
2022-07-25 20:35:52 -0300

    Create Main.java

---
+++ Lista Nota/Ex 01/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    
+   /*
+    
+    Pergunta: Explique o que são e qual a utilidade de Estrutura de Dados
+    
+    Resposta: Estruturas de dados são técnicas utilizadas para organizar/estruturar os dados,
+    de modo que seja possível inserir, remover e buscar informações
+    
+    */
+    
+}
